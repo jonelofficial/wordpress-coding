8264c6bc665fbee97c5c6cf29f2dca324f5848eb
Jonel Home PC <it.jonelfignacio@gmail.com>
2023-04-07 18:27:30 +0800

    on pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,8 +1,8 @@
-<!-- This page run when click a post -->
+<!-- This page run when redirect to new page -->
 <?= get_header() ?>
 
 <?php while (have_posts()) : the_post() ?>
-    <h2>This is a single page of <?= the_title() ?></h2>
+    <h2> This is a new page of <?= the_title() ?></h2>
     <p> <?= the_content() ?> </p>
     <hr>
 <?php endwhile ?>
